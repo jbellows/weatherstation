81e3aa0beacac5f35a6d4541b1e4a7f99395af9f
jbellows <jared@thebellows.info>
2014-08-15 14:59:45 -0600

    Added some of the interrupt code and also the beginning of the RTC.

--- WindStation.ino
+++ WindStation.ino
@@ -1,26 +1,67 @@
+#include <DS3232RTC.h>
+#include <avr/sleep.h>
+
+byte windRevolutions = 0;
+uint16_t secondsCounter = 0;
+
 void setup() {
- // Open serial interface 
+  Serial.begin(115200);
+  
+  RTC.squareWave(SQWAVE_1_HZ);
+
+  pinMode(A0, OUTPUT);
+  digitalWrite(A0, HIGH);
+ 
+  // Enable the ADC0 interrupt for pin I/O level change
+  cli();
+  PCICR  = 0b00000110;
+  PCMSK1 = 0b00000001;
+  PCMSK2 = 0b00000100;
+  sei();
 }
 
 void loop() {
-  // Check if it's been five minutes
-  //   Write records out to SD card
-  //   Reset the EEPROM pointer to beginning
-  //   Calibrate compass
-  // Check if it's been one minute since last wind heading read
-  //   Get a wind heading
-  // Power Down MCU
+  Serial.println(windRevolutions >> 1);
+  windRevolutions = 0;
+  delay(1000);
+  if (secondsCounter >= 300) {
+    //   Write records out to SD card
+    //   Reset the EEPROM pointer to beginning
+    //   Calibrate compass
+    secondsCounter = 0;
+  }
+  
+  if ((secondsCounter % 60) == 0) {
+    analogRead(A2);
+  }
+ 
+  sleepNow();
+}
+
+void sleepNow() {
+  set_sleep_mode(SLEEP_MODE_PWR_DOWN);
+
+  cli();
+  sleep_enable();
+  sei();
+
+  sleep_cpu();
+  sleep_disable();
 }
 
 // Interrupt from wind speed
-//   Increment a counter (INT0 / pin 2)
+ISR(PCINT1_vect) {
+  // Increment a counter
+  windRevolutions++;
+}
 
 // Interrupt from RTC every 500ms
-//   If a rising edge pin change
-//     Insert second record into EEPROM
-//     Reset wind speed counter
-//     Increment overall second counter
-//   Else
-//     Power Down MCU
-//   (PCINT / pin ?)
-
+ISR(PCINT2_vect) {
+  if(digitalRead(2) == HIGH) {
+    // Insert second record into EEPROM
+    windRevolutions = 0;
+    secondsCounter++;
+  } else {
+    sleepNow();
+  }
+}
